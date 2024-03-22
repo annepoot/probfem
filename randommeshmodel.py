38c6ddbb25b18a41f40c2138256d4e085f42b89c
Anne Poot <a.poot-1@tudelft.nl>
2024-03-22 10:48:14 +0100

    Implemented WRITEMESH action in RandomMeshModel

--- randommeshmodel.py
+++ randommeshmodel.py
@@ -1,17 +1,9 @@
 import numpy as np
+import os
 
 from myjive.names import GlobNames as gn
 from myjive.model.model import Model
-from myjive.fem import to_xnodeset
-
-ELEMENTS = "elements"
-EA = "EA"
-k = "k"
-RHOA = "rhoA"
-SHAPE = "shape"
-TYPE = "type"
-INTSCHEME = "intScheme"
-DOFTYPES = ["dx"]
+from myjive.util.proputils import optarg
 
 
 class RandomMeshModel(Model):
@@ -19,14 +11,46 @@ class RandomMeshModel(Model):
         nodes = self._perturb_nodes(nodes, globdat)
         return nodes
 
+    def WRITEMESH(self, globdat, fname, ftype, **kwargs):
+        if "manual" in ftype:
+            self._write_mesh(globdat, fname)
+        else:
+            raise ValueError("Invalid file type passed to WRITEMESH")
+
     def configure(self, globdat, **props):
-        pass
+        # get props
+        self._seed = optarg(self, props, "seed")
+
+        self._rng = np.random.default_rng(seed=self._seed)
 
     def _perturb_nodes(self, nodes, globdat):
-
         for node in nodes:
             coords = node.get_coords()
-            coords += np.random.uniform(-0.01, 0.01)
+            coords += self._rng.uniform(-0.01, 0.01)
             node.set_coords(coords)
 
         return nodes
+
+    def _write_mesh(self, globdat, fname):
+        nodes = globdat[gn.NSET]
+        elems = globdat[gn.ESET]
+
+        path, file = os.path.split(fname)
+        if len(path) > 0 and not os.path.isdir(path):
+            os.makedirs(path)
+
+        with open(fname, "w") as file:
+            file.write("nodes (ID, x, [y], [z])\n")
+            for inode, node in enumerate(nodes):
+                node_id = nodes.get_node_id(inode)
+                coords = node.get_coords()
+                file.write("{} ".format(node_id))
+                file.write(" ".join(["{}".format(coord) for coord in coords]))
+                file.write("\n")
+
+            file.write("elements (node#1, node#2, [node#3, ...])\n")
+            for ielem, elem in enumerate(elems):
+                inodes = elems.get_elem_nodes(ielem)
+                node_ids = nodes.get_node_ids(inodes)
+                file.write(" ".join(["{}".format(node_id) for node_id in node_ids]))
+                file.write("\n")
