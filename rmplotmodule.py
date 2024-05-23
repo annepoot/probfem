8f236afd09234792594e20fe6f4dcf385221dc45
Anne Poot <a.poot-1@tudelft.nl>
2024-05-23 10:32:14 +0200

    Added femField parameter to interpolation superscript

--- rmplotmodule.py
+++ rmplotmodule.py
@@ -5,7 +5,7 @@ from myjive.names import GlobNames as gn
 from myjive.app import Module
 from myjive.fem import XPointSet
 from myjive.util import Table
-from myjive.util.proputils import check_value
+from myjive.util.proputils import check_value, get_recursive, split_key
 
 
 class RMPlotModule(Module):
@@ -20,6 +20,7 @@ class RMPlotModule(Module):
         figure={},
         exact={},
         fem={},
+        femField=None,
         perturbed={},
         save={}
     ):
@@ -31,6 +32,7 @@ class RMPlotModule(Module):
         self._figprops = figure
         self._exactprops = exact
         self._femprops = fem
+        self._femfield = femField
         self._pertprops = perturbed
         self._saveprops = save
 
@@ -109,29 +111,33 @@ class RMPlotModule(Module):
         for model in models:
             table = model.GETEXACTTABLE(self._field, table, globdat, points)
 
-        x_exact = points.get_coords().flatten()
         field_exact = table[self._comp]
+        x_exact = points.get_coords().flatten()
 
         return x_exact, field_exact
 
     def _get_fem_solution(self, globdat):
-        if self._plottype == "node":
-            nodecount = len(globdat[gn.NSET])
-            table = Table(size=nodecount)
-            tbwts = np.zeros(nodecount)
-            models = self.get_relevant_models("GETTABLE", globdat[gn.MODELS])
-            for model in models:
-                table, tbwts = model.GETTABLE(self._field, table, tbwts, globdat)
-        elif self._plottype == "elem":
-            table = Table(size=len(globdat[gn.ESET]))
-            models = self.get_relevant_models("GETELEMTABLE", globdat[gn.MODELS])
-            for model in models:
-                table = model.GETELEMTABLE(self._field, table, globdat)
+        if self._femfield is None:
+            if self._plottype == "node":
+                nodecount = len(globdat[gn.NSET])
+                table = Table(size=nodecount)
+                tbwts = np.zeros(nodecount)
+                models = self.get_relevant_models("GETTABLE", globdat[gn.MODELS])
+                for model in models:
+                    table, tbwts = model.GETTABLE(self._field, table, tbwts, globdat)
+            elif self._plottype == "elem":
+                table = Table(size=len(globdat[gn.ESET]))
+                models = self.get_relevant_models("GETELEMTABLE", globdat[gn.MODELS])
+                for model in models:
+                    table = model.GETELEMTABLE(self._field, table, globdat)
+            else:
+                assert False
+
+            field_fem = table[self._comp]
         else:
-            assert False
+            field_fem = get_recursive(globdat, split_key(self._femfield))
 
         x_fem = self._get_x(globdat)
-        field_fem = table[self._comp]
 
         return x_fem, field_fem
 
