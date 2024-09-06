1648639c7caff3d4856ba32964ff51af8db48895
Anne Poot <a.poot-1@tudelft.nl>
2024-09-06 19:25:57 +0200

    Ported BFEM to this repo (with a whole new setup)

---
+++ bfem/boundaryobservationmodel.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from myjive.names import GlobNames as gn
+from myjive.model.model import Model
+
+
+class BoundaryObservationModel(Model):
+    def GETOBSERVATIONOPERATOR(self, globdat):
+        Phi = self._get_phi(globdat)
+        return Phi.T
+
+    def GETMEASUREMENTS(self, globdat):
+        measurements = self._get_measurements(globdat)
+        return measurements
+
+    @Model.save_config
+    def configure(self, globdat):
+        pass
+
+    def _get_measurements(self, globdat):
+        fglobdat = globdat["fine"]
+        dofs, vals = fglobdat[gn.CONSTRAINTS].get_constraints()
+        return np.array(vals)
+
+    def _get_phi(self, globdat):
+        fglobdat = globdat["fine"]
+        dofs, vals = fglobdat[gn.CONSTRAINTS].get_constraints()
+        dof_count = fglobdat[gn.DOFSPACE].dof_count()
+
+        Phi = np.zeros((dof_count, len(dofs)))
+
+        for i, dof in enumerate(dofs):
+            Phi[dof, i] = 1.0
+
+        return Phi
