a597466781c8fed11059ade3a2c7563899c052a3
Anne Poot <a.poot-1@tudelft.nl>
2024-11-27 10:33:14 +0100

    Implemented learning of StatFEM hyperparameters

---
+++ inverse/hyper-plots.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+variables = {
+    "fem": ["sigma_e"],
+    "statfem": ["rho", "l_d", "sigma_d", "sigma_e"],
+}
+
+labels_by_var = {
+    "rho": r"$\rho$",
+    "l_d": r"$\l_d$",
+    "sigma_d": r"$\sigma_d$",
+    "sigma_e": r"$\sigma_e$",
+}
+
+N_burn = 5000
+N_filter = 1
+
+for fem_type in ["fem", "statfem"]:
+    fname = "samples-{}-hyper.csv".format(fem_type)
+    df = pd.read_csv(fname)
+    df["n_elem"] = df["n_elem"].astype(str)
+
+    for var in variables[fem_type]:
+        plt.figure()
+        ax = sns.kdeplot(
+            df[(df["sample"] >= N_burn)], x=var, hue="n_elem", log_scale=True
+        )
+        ax.set_xlabel(labels_by_var[var])
+        plt.show()
+
+    df = df[df["n_elem"] == "40"]
+    df = df[df["sample"] > N_burn]
+
+    for i, var1 in enumerate(variables[fem_type]):
+        for var2 in variables[fem_type][i + 1 :]:
+            plt.figure()
+            ax = sns.scatterplot(
+                df, x=var1, y=var2, hue="n_elem", alpha=0.5, marker=".", edgecolor=None
+            )
+            ax.set_xlabel(labels_by_var[var1])
+            ax.set_ylabel(labels_by_var[var2])
+            plt.show()
---
+++ inverse/props/rwm_fem_hyper_props.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from .rwm_fem_props import get_rwm_fem_props
+from probability.univariate import Gaussian
+from probability import ParametrizedLikelihood, IndependentJoint
+
+
+def get_rwm_fem_hyper_props(*, std_corruption, n_rep_obs):
+    rwm_fem_hyper_props = get_rwm_fem_props(
+        std_corruption=std_corruption, sigma_e=1.0, n_rep_obs=n_rep_obs
+    )
+
+    rwm_fem_hyper_props["target"]["prior"] = {
+        "type": IndependentJoint,
+        "distributions": [
+            rwm_fem_hyper_props["target"].pop("prior"),
+            {"type": Gaussian, "mean": np.log(1e-4), "std": np.log(1e1)},
+        ],
+    }
+    rwm_fem_hyper_props["target"]["likelihood"]["type"] = ParametrizedLikelihood
+    rwm_fem_hyper_props["target"]["likelihood"]["hyperparameters"] = ["noise.std"]
+    rwm_fem_hyper_props["proposal"] = {
+        "type": IndependentJoint,
+        "distributions": [
+            rwm_fem_hyper_props.pop("proposal"),
+            {"type": Gaussian, "mean": np.log(1e-4), "std": np.log(1e1)},
+        ],
+    }
+    rwm_fem_hyper_props["startValue"] = np.zeros(5)
+
+    return rwm_fem_hyper_props
