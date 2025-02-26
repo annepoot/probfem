8d4092fd9fe20b3eca6f101757a5556549e3145a
Anne Poot <a.poot-1@tudelft.nl>
2025-02-26 11:19:12 +0100

    Plotting cleanups

--- experiments/inverse/kl_bar/plots/pairgrid-plots.py
+++ experiments/inverse/kl_bar/plots/pairgrid-plots.py
@@ -1,4 +1,6 @@
+import os
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
@@ -22,6 +24,13 @@ refs_by_var = {
     "xi_4": 0.25,
 }
 
+plims_by_fem_type = {
+    "fem": 150,
+    "rmfem": 25,
+    "bfem": 25,
+    "statfem": 25,
+}
+
 
 def lims_by_var(width):
     lims_by_var = {}
@@ -32,64 +41,68 @@ def lims_by_var(width):
 
 
 labels_by_var = {
-    "xi_1": r"$\theta_1$",
-    "xi_2": r"$\theta_2$",
-    "xi_3": r"$\theta_3$",
-    "xi_4": r"$\theta_4$",
+    "xi_1": r"$\xi_1$",
+    "xi_2": r"$\xi_2$",
+    "xi_3": r"$\xi_3$",
+    "xi_4": r"$\xi_4$",
 }
 
-title_map = {
-    "fem": "FEM",
-    "bfem": "BFEM",
-    "statfem": "StatFEM",
-    "rmfem": "RM-FEM",
-}
-
-N_filter = 100
+width = 0.10
 N_burn = 10000
+N_filter = 20
+N_elem_range = [10, 20, 40]
+
+
+def custom_kde_2d(x, y, *, color, label, **kwargs):
+    sns.kdeplot(x=x, y=y, color=color, label=label, **kwargs)
+
+
+plt.rc("text", usetex=True)  # use latex for text
+plt.rcParams["text.latex.preamble"] = r"\usepackage{xfrac}"
 
 for fem_type in ["fem", "bfem", "rmfem", "statfem"]:
-    for width in [0.1]:
-        fname = "../samples-{}.csv".format(fem_type)
-        df = read_csv_from(fname, "xi_1,xi_2,xi_3,xi_4")
-        df = df[(df["sample"] >= N_burn) & (df["sample"] % N_filter == 0)]
-        df["n_elem"] = df["n_elem"].astype(str)
+    fname = os.path.join("..", "samples-{}.csv".format(fem_type))
+    df = read_csv_from(fname, "xi_1,xi_2,xi_3,xi_4")
+    df = df[(df["sample"] >= N_burn) & (df["sample"] % N_filter == 0)]
+    df = df[df["n_elem"].isin(N_elem_range)]
+    df["n_elem"] = df["n_elem"].astype(str)
+    df["h"] = r"\sfrac{1}{" + df["n_elem"] + "}"
 
-        grid = sns.PairGrid(
-            data=df, vars=variables, hue="n_elem", diag_sharey=False, height=1.5
-        )
-        grid.map_upper(sns.scatterplot, alpha=0.5, marker=".", edgecolor=None)
-        grid.map_lower(sns.kdeplot)
-        grid.map_diag(sns.kdeplot)
+    grid = sns.PairGrid(data=df, vars=variables, hue="h", diag_sharey=True, height=1.5)
+    grid.map_upper(sns.scatterplot, alpha=0.5, marker=".", linewidths=0.0)
+    grid.map_lower(custom_kde_2d, levels=5)
+    grid.map_diag(sns.kdeplot)
 
-        nvar = len(variables)
-        for i, var in enumerate(variables):
-            lims = lims_by_var(width)[var]
-            grid.axes[(i + 1) % nvar, i].set_xlim(lims)
-            grid.axes[i, (i + 1) % nvar].set_ylim(lims)
-            grid.axes[(i + 1) % nvar, i].set_xticks(np.linspace(lims[0], lims[1], 3))
-            grid.axes[i, (i + 1) % nvar].set_yticks(np.linspace(lims[0], lims[1], 3))
+    nvar = len(variables)
+    for i, var in enumerate(variables):
+        lims = lims_by_var(width)[var]
+        grid.axes[(i + 1) % nvar, i].set_xlim(lims)
+        grid.axes[i, (i + 1) % nvar].set_ylim(lims)
+        grid.axes[(i + 1) % nvar, i].set_xticks(np.linspace(lims[0], lims[1], 3))
+        grid.axes[i, (i + 1) % nvar].set_yticks(np.linspace(lims[0], lims[1], 3))
 
-        for i, xvar in enumerate(variables):
-            for j, yvar in enumerate(variables):
-                xlabel = labels_by_var[xvar]
-                ylabel = labels_by_var[yvar]
-                grid.axes[j, i].xaxis.set_label_text(xlabel)
-                grid.axes[j, i].yaxis.set_label_text(ylabel)
+    for i in range(len(variables)):
+        grid.diag_axes[i].set_ylim((0, plims_by_fem_type[fem_type]))
 
-                xref = refs_by_var[xvar]
-                yref = refs_by_var[yvar]
-                if i == j:
-                    grid.axes[j, i].axvline(x=xref, color="k", label="ref", zorder=2)
-                else:
-                    grid.axes[j, i].scatter(
-                        [xref], [yref], color="k", label="ref", zorder=2
-                    )
+    for i, xvar in enumerate(variables):
+        for j, yvar in enumerate(variables):
+            xlabel = labels_by_var[xvar]
+            ylabel = labels_by_var[yvar]
+            grid.axes[j, i].xaxis.set_label_text(xlabel)
+            grid.axes[j, i].yaxis.set_label_text(ylabel)
 
-        grid.add_legend()
-        grid.fig.subplots_adjust(top=0.95)
-        grid.fig.suptitle(title_map[fem_type])
-        # grid.savefig(
-        #     fname="img/pairgrid-plot_noise-{}_width-{}".format(noise, int(width * 100)),
-        #     dpi=300,
-        # )
+            xref = refs_by_var[xvar]
+            yref = refs_by_var[yvar]
+            if i == j:
+                grid.axes[j, i].axvline(x=xref, color="k", label="ref", zorder=2)
+            else:
+                grid.axes[j, i].scatter(
+                    [xref], [yref], color="k", label="ref", zorder=2
+                )
+
+    labels = [r"$\sfrac{1}{10}$", r"$\sfrac{1}{20}$", r"$\sfrac{1}{40}$"]
+
+    grid.add_legend(title=r"$h$")
+    fontsize = "12"
+    plt.setp(grid.legend.get_texts(), fontsize=fontsize)
+    plt.setp(grid.legend.get_title(), fontsize=fontsize)
