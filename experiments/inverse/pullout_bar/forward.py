Fixed bug in u-exact computation (forgot to divide f by E to get du/dx)

experiments/inverse/pullout_bar/forward.py
@@ -30,8 +30,9 @@ def u_exact(x):
     f = props["model"]["model"]["neum"]["initLoad"]
 
     nu = np.sqrt(k / E)
+    eps = f / E
 
-    A = f / (nu * (np.exp(nu) - np.exp(-nu)))
+    A = eps / (nu * (np.exp(nu) - np.exp(-nu)))
 
     return A * (np.exp(nu * x) + np.exp(-nu * x))
 

experiments/inverse/pullout_bar/props/rwm_fem_props.py
@@ -19,8 +19,9 @@ def get_rwm_fem_target(*, elems, std_corruption, sigma_e):
     E = fem_props["model"]["model"]["elastic"]["material"]["E"]
     f = fem_props["model"]["model"]["neum"]["initLoad"]
     nu = np.sqrt(k / E)
+    eps = f / E
 
-    u_exact = f * (np.exp(nu) + np.exp(-nu)) / (nu * (np.exp(nu) - np.exp(-nu)))
+    u_exact = eps * (np.exp(nu) + np.exp(-nu)) / (nu * (np.exp(nu) - np.exp(-nu)))
     ground_truth = u_exact
 
     # generate repeated noisy observations