83e8b2ec51741f22978c58249972a10cfcc6e041
Anne Poot <a.poot-1@tudelft.nl>
2025-05-13 14:27:37 +0200

    Created separate get_or_calc caching functions (which take care of the dependencies and precomputed quantities)

--- experiments/inverse/frp_damage/generate_truth.py
+++ experiments/inverse/frp_damage/generate_truth.py
@@ -1,226 +1,66 @@
-import os
 import numpy as np
 
-from myjive.fem import Tri3Shape
-from myjivex.util import QuickViewer, ElemViewer
-
-from fem.jive import CJiveRunner
-from fem.meshing import read_mesh
-
-from experiments.inverse.frp_damage.props import get_fem_props
-from experiments.inverse.frp_damage import caching, misc, params
-
-rve_size = params.geometry_params["rve_size"]
-n_fiber = params.geometry_params["n_fiber"]
-r_fiber = params.geometry_params["r_fiber"]
-tol = params.geometry_params["tol_fiber"]
-seed = params.geometry_params["seed_fiber"]
+from experiments.inverse.frp_damage import caching, params
 
 #######################
 # get fiber locations #
 #######################
-
-name = "fibers"
-dependencies = {"nfib": n_fiber}
-path = caching.get_cache_fpath(name, dependencies)
-
-if caching.is_cached(path):
-    print("Reading fibers from cache")
-    fibers = caching.read_cache(path)
-else:
-    print("Computing fibers")
-    fibers = misc.calc_fibers(n=n_fiber, a=rve_size, r=r_fiber, tol=tol, seed=seed)
-    print("Writing fibers to cache")
-    caching.write_cache(path, fibers)
+fibers = caching.get_or_calc_fibers()
 
 ############
 # get mesh #
 ############
-
 h = 0.002
-fname = "meshes/rve_h-{:.3f}_nfib-{}.msh".format(h, n_fiber)
-
-if not os.path.exists(fname):
-    print("Computing mesh")
-    misc.create_mesh(fibers=fibers, a=rve_size, r=r_fiber, h=h, fname=fname)
+nodes, elems, egroups = caching.get_or_calc_mesh(h=h)
 
 ##########################
 # get integration points #
 ##########################
 
-nodes, elems, groups = read_mesh(fname, read_groups=True)
-egroup = groups["matrix"]
-shape = Tri3Shape("Gauss3")
-
-name = "ipoints"
-dependencies = {"nfib": n_fiber, "h": h}
-path = caching.get_cache_fpath(name, dependencies)
-
-if caching.is_cached(path):
-    print("Reading ipoints from cache")
-    ipoints = caching.read_cache(path)
-else:
-    print("Computing ipoints")
-    ipoints = misc.calc_integration_points(egroup, shape)
-    print("Writing ipoints to cache")
-    caching.write_cache(path, ipoints)
+egroup = egroups["matrix"]
+ipoints = caching.get_or_calc_ipoints(egroup=egroup, h=h)
 
 #######################
 # get fiber distances #
 #######################
 
-name = "distances"
-dependencies = {"nfib": n_fiber, "h": h}
-path = caching.get_cache_fpath(name, dependencies)
-
-if caching.is_cached(path):
-    print("Reading distances from cache")
-    distances = caching.read_cache(path)
-else:
-    print("Computing distances")
-    distances = np.zeros(ipoints.shape[0])
-    for ip, ipoint in enumerate(ipoints):
-        fiber, dist = misc.calc_closest_fiber(ipoint, fibers, 1.0)
-        distances[ip] = dist - r_fiber
-    assert 0.0 < np.min(distances) < 0.1 * h
-    print("Writing distances to cache")
-    caching.write_cache(path, distances)
+distances = caching.get_or_calc_distances(egroup=egroup, h=h)
 
 ###################
 # get stiffnesses #
 ###################
 
-backdoor = {}
-backdoor["xcoord"] = ipoints[:, 0]
-backdoor["ycoord"] = ipoints[:, 1]
-backdoor["e"] = np.zeros(ipoints.shape[0])
-
-E = params.material_params["E_matrix"]
-alpha = params.material_params["alpha"]
-beta = params.material_params["beta"]
-c = params.material_params["c"]
-d = params.material_params["d"]
-
-for ip, ipoint in enumerate(ipoints):
-    dist = distances[ip]
-    sat = misc.saturation(dist, alpha, beta, c)
-    dam = misc.damage(sat, d)
-    backdoor["e"][ip] = E * (1 - dam)
-
-elem_stiffness = np.zeros(len(elems))
-
-for group_name, egroup in groups.items():
-    if group_name == "matrix":
-        for ie, ielem in enumerate(egroup):
-            ip_stiffness = backdoor["e"][3 * ie : 3 * (ie + 1)]
-            elem_stiffness[ielem] = np.mean(ip_stiffness)
-    elif group_name == "fiber":
-        ielems = egroup.get_indices()
-        elem_stiffness[ielems] = 0
-    else:
-        assert False
+stiffnesses = caching.get_or_calc_true_stiffnesses(egroup=egroup, h=h)
 
 #####################
 # get displacements #
 #####################
 
-props = get_fem_props()
-
-print("Computing solution")
-
-jive = CJiveRunner(props, elems=elems, egroups=groups)
-globdat = jive(**backdoor)
-
-QuickViewer(
-    globdat["state0"],
-    globdat,
-    comp=0,
-)
-ElemViewer(
-    elem_stiffness,
-    globdat,
-    maxcolor=E,
-    title=r"stiffness, $N_e = {}$".format(len(elems)),
-)
-
+displacements = caching.get_or_calc_true_displacements(egroups=egroups, h=h)
 
 #########################
 # get speckle locations #
 #########################
 
-obs_size = params.geometry_params["obs_size"]
-n_speckle = params.geometry_params["n_speckle"]
-r_speckle = params.geometry_params["r_speckle"]
-tol = params.geometry_params["tol_speckle"]
-seed = seed = params.geometry_params["seed_speckle"]
-
-name = "speckles"
-dependencies = {"nobs": n_speckle}
-path = caching.get_cache_fpath(name, dependencies)
-
-if caching.is_cached(path):
-    print("Reading speckles from cache")
-    speckles = caching.read_cache(path)
-else:
-    print("Computing speckles")
-    speckles = misc.calc_fibers(
-        n=n_speckle, a=obs_size, r=r_speckle, tol=tol, seed=seed
-    )
-    print("Writing speckles to cache")
-    caching.write_cache(path, speckles)
+speckles = caching.get_or_calc_speckles()
 
 #########################
 # get speckle neighbors #
 #########################
 
-name = "connectivity"
-dependencies = {"nobs": n_speckle}
-path = caching.get_cache_fpath(name, dependencies)
-
-if caching.is_cached(path):
-    print("Reading connectivity from cache")
-    connectivity = caching.read_cache(path)
-else:
-    print("Computing connectivity")
-    connectivity = misc.calc_connectivity(speckles)
-    print("Writing connectivity to cache")
-    caching.write_cache(path, connectivity)
+connectivity = caching.get_or_calc_connectivity()
 
 ############################
 # get observation operator #
 ############################
 
-name = "observer"
-dependencies = {"nobs": n_speckle, "h": h, "nfib": n_fiber, "sparse": True}
-path = caching.get_cache_fpath(name, dependencies)
-
-if caching.is_cached(path):
-    print("Reading observer from cache")
-    obs_operator = caching.read_cache(path)
-else:
-    print("Computing observer")
-    dofs = globdat["dofSpace"]
-    obs_operator = misc.calc_observer(speckles, connectivity, elems, dofs, shape)
-    print("Writing observer to cache")
-    caching.write_cache(path, obs_operator)
+obs_operator = caching.get_or_calc_obs_operator(elems=elems, h=h)
 
 ####################
 # get ground truth #
 ####################
 
-name = "truth"
-dependencies = {"nobs": n_speckle, "h": h, "nfib": n_fiber}
-path = caching.get_cache_fpath(name, dependencies)
-
-if caching.is_cached(path):
-    print("Reading truth from cache")
-    truth = caching.read_cache(path)
-else:
-    print("Computing truth")
-    truth = obs_operator @ globdat["state0"]
-    print("Writing truth to cache")
-    caching.write_cache(path, truth)
-
+truth = caching.get_or_calc_true_observations(egroups=egroups, h=h)
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -240,6 +80,7 @@ for i, edge in enumerate(connectivity):
     ax.plot([coord1[0], coord2[0]], [coord1[1], coord2[1]], color=color)
 
 ax.set_aspect("equal")
+obs_size = params.geometry_params["obs_size"]
 ax.set_xlim((-obs_size, obs_size))
 ax.set_ylim((-obs_size, obs_size))
 plt.show()
--- experiments/inverse/frp_damage/prior.py
+++ experiments/inverse/frp_damage/prior.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from myjive.fem import Tri3Shape
 from fem.jive import CJiveRunner
-from fem.meshing import read_mesh
 from probability.multivariate import Gaussian
 from probability.process import GaussianProcess, ZeroMeanFunction, SquaredExponential
 
@@ -16,12 +14,6 @@ r_fiber = params.geometry_params["r_fiber"]
 tol = params.geometry_params["tol_fiber"]
 seed = params.geometry_params["seed_fiber"]
 h = 0.05
-
-name = "distances"
-dependencies = {"nfib": n_fiber, "h": h}
-path = caching.get_cache_fpath(name, dependencies)
-print("Reading distances from cache")
-distances = caching.read_cache(path)
 
 x = np.linspace(0.0, 0.2, 101)
 
@@ -62,35 +54,14 @@ ax.plot(x, damage, color="k")
 ax.set_title("Prior samples of damage field")
 plt.show()
 
-##########################
-# get integration points #
-##########################
+#########################
+# get precomputed stuff #
+#########################
 
-props = get_fem_props()
-fname = "meshes/rve_h-{:.3f}_nfib-{}.msh".format(h, n_fiber)
-nodes, elems, groups = read_mesh(fname, read_groups=True)
-egroup = groups["matrix"]
-shape = Tri3Shape("Gauss3")
-
-name = "ipoints"
-dependencies = {"nfib": n_fiber, "h": h}
-path = caching.get_cache_fpath(name, dependencies)
-print("Reading ipoints from cache")
-ipoints = caching.read_cache(path)
-
-#######################
-# get fiber distances #
-#######################
-
-name = "distances"
-dependencies = {"nfib": n_fiber, "h": h}
-path = caching.get_cache_fpath(name, dependencies)
-print("Reading distances from cache")
-distances = caching.read_cache(path)
-
-###################
-# get stiffnesses #
-###################
+nodes, elems, egroups = caching.get_or_calc_mesh(h=h)
+egroup = egroups["matrix"]
+ipoints = caching.get_or_calc_ipoints(egroup=egroup, h=h)
+distances = caching.get_or_calc_distances(egroup=egroup, h=h)
 
 backdoor = {}
 backdoor["xcoord"] = ipoints[:, 0]
@@ -98,46 +69,6 @@ backdoor["ycoord"] = ipoints[:, 1]
 backdoor["e"] = np.zeros(ipoints.shape[0])
 
 from myjivex.util import ElemViewer
-
-
-def calc_strains(globdat):
-    state0 = globdat["state0"]
-    elems = globdat["elemSet"]
-    dofs = globdat["dofSpace"]
-    shape = globdat["shape"]
-
-    nodes = elems.get_nodes()
-
-    ipcount = shape.ipoint_count()
-    nodecount = shape.node_count()
-    strains_xx = np.zeros((len(elems), ipcount))
-    strains_yy = np.zeros((len(elems), ipcount))
-    strains_xy = np.zeros((len(elems), ipcount))
-
-    for ielem, inodes in enumerate(elems):
-        idofs = dofs.get_dofs(inodes, ["dx", "dy"])
-        eldisp = state0[idofs]
-
-        coords = nodes[inodes]
-        grads, wts = shape.get_shape_gradients(coords)
-
-        for ip in range(ipcount):
-            B = np.zeros((3, 2 * nodecount))
-
-            for n in range(nodecount):
-                B[0, 2 * n + 0] = grads[ip, 0, n]
-                B[1, 2 * n + 1] = grads[ip, 1, n]
-                B[2, 2 * n + 0] = grads[ip, 1, n]
-                B[2, 2 * n + 1] = grads[ip, 0, n]
-
-            ipstrain = B @ eldisp
-
-            strains_xx[ielem, ip] = ipstrain[0]
-            strains_yy[ielem, ip] = ipstrain[1]
-            strains_xy[ielem, ip] = ipstrain[2]
-
-    return strains_xx, strains_yy, strains_xy
-
 
 for damage in damage_samples:
     for ip, ipoint in enumerate(ipoints):
@@ -157,7 +88,7 @@ for damage in damage_samples:
 
     elem_stiffness = np.zeros(len(elems))
 
-    for group_name, egroup in groups.items():
+    for group_name, egroup in egroups.items():
         if group_name == "matrix":
             for ie, ielem in enumerate(egroup):
                 ip_stiffness = backdoor["e"][3 * ie : 3 * (ie + 1)]
@@ -168,10 +99,9 @@ for damage in damage_samples:
         else:
             assert False
 
-    jive = CJiveRunner(props, elems=elems, egroups=groups)
+    props = get_fem_props()
+    jive = CJiveRunner(props, elems=elems, egroups=egroups)
     globdat = jive(**backdoor)
-
-    eps_xx, eps_yy, gamma_xy = calc_strains(globdat)
 
     ElemViewer(
         elem_stiffness,
@@ -179,6 +109,9 @@ for damage in damage_samples:
         maxcolor=E_matrix,
         title=r"stiffness, $N_e = {}$".format(len(elems)),
     )
+
+    # eps_xx, eps_yy, gamma_xy = misc.calc_strains(globdat)
+
     # ElemViewer(
     #     eps_xx[:, 0],
     #     globdat,
