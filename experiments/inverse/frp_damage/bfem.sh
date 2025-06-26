85496e7ba0e9bfac61440900525ebc64fabaf34e
Anne Poot <a.poot-1@tudelft.nl>
2025-06-26 15:02:55 +0200

    Sif file generation

--- experiments/inverse/frp_damage/bfem.sh
+++ experiments/inverse/frp_damage/bfem.sh
@@ -6,5 +6,5 @@ conda activate probfem
 # conda develop /home/apoot1/git/myjive
 export PYTHONPATH="/home/apoot1/git/probfem:$PYTHONPATH"
 export PYTHONPATH="/home/apoot1/git/myjive:$PYTHONPATH"
-python inverse_bfem_blue.py $1 $2
+python -u inverse_bfem_blue.py $1 $2
 
