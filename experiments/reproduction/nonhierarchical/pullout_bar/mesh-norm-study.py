819a78836bba4c509c555a2bc2f2968515ceef7a
Anne Poot <a.poot-1@tudelft.nl>
2025-08-19 14:14:07 +0200

    Convergence study plots

--- experiments/reproduction/nonhierarchical/pullout_bar/mesh-norm-study.py
+++ experiments/reproduction/nonhierarchical/pullout_bar/mesh-norm-study.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.ticker import LogLocator, LogFormatter, NullFormatter
+import seaborn as sns
 
 from fem.meshing import mesh_interval_with_line2
 
@@ -29,7 +29,7 @@ else:
 # options: exact, hierarchical, dual, random
 n_obs = 4
 n_refs = np.array([4, 8, 16, 32, 64, 128, 256])
-n_refs_hierarchical = n_refs[1:]
+n_refs_hierarchical = n_refs
 n_refs_random = np.concatenate([n + n // 4 * np.array([0, 1, 2, 3]) for n in n_refs])
 n_refs_random = np.concatenate([np.array([1, 2, 3]), n_refs_random])
 n_refs_optimal = np.array([1, 2, 3, 4, 5, 7, 9, 13, 17, 33, 65, 129, 257])
@@ -39,11 +39,13 @@ obs_nodes, obs_elems = mesh_interval_with_line2(n=n_elem)
 
 hierarchical_norms = np.zeros_like(n_refs_hierarchical, dtype=float)
 dual_norms = np.zeros_like(n_refs, dtype=float)
+left_norms = np.zeros_like(n_refs, dtype=float)
+right_norms = np.zeros_like(n_refs, dtype=float)
 random_norms = np.zeros((len(n_refs_random), n_seed), dtype=float)
 dropout_norms = np.zeros((len(n_refs_random), n_seed), dtype=float)
 optimal_norms = np.zeros_like(n_refs_optimal, dtype=float)
 
-for ref_type in ["exact", "hierarchical", "dual", "optimal", "random"]:
+for ref_type in ["exact", "hierarchical", "dual", "left", "right", "optimal", "random"]:
     if ref_type == "exact":
         ref_nodes, ref_elems = mesh_interval_with_line2(n=2048)
         exact_norm = misc.calc_norm(obs_elems, ref_elems)
@@ -56,6 +58,16 @@ for ref_type in ["exact", "hierarchical", "dual", "optimal", "random"]:
             ref_nodes, ref_elems = mesh_interval_with_line2(n=n_ref)
             ref_nodes, ref_elems = misc.dual_mesh(ref_elems)
             dual_norms[i] = misc.calc_norm(obs_elems, ref_elems)
+    elif ref_type == "left":
+        for i, n_ref in enumerate(n_refs):
+            ref_nodes, ref_elems = mesh_interval_with_line2(n=n_ref + 1)
+            ref_nodes._data[: n_ref + 1, 0] = np.linspace(0, 0.5, n_ref + 1)
+            left_norms[i] = misc.calc_norm(obs_elems, ref_elems)
+    elif ref_type == "right":
+        for i, n_ref in enumerate(n_refs):
+            ref_nodes, ref_elems = mesh_interval_with_line2(n=n_ref + 1)
+            ref_nodes._data[1 : n_ref + 2, 0] = np.linspace(0.5, 1.0, n_ref + 1)
+            right_norms[i] = misc.calc_norm(obs_elems, ref_elems)
     elif ref_type == "dropout":
         for i, n_ref in enumerate(n_refs):
             for j in range(n_seed):
@@ -77,56 +89,121 @@ for ref_type in ["exact", "hierarchical", "dual", "optimal", "random"]:
         assert False
 
 
+highlight = sns.color_palette("rocket_r", n_colors=8)[int(np.log2(n_obs))]
+major_ticks = 2 ** np.arange(0, 10)
+minor_ticks = np.array([3])
+for offset in [1.25, 1.50, 1.75]:
+    minor_ticks = np.concatenate((minor_ticks, major_ticks[2:-1] * offset))
+
+
 fig, ax = plt.subplots()
+
+for n_ref, norm in zip(n_refs_hierarchical, hierarchical_norms):
+    c = highlight if n_ref == 8 else "0.3"
+    label = "hierarchical" if n_ref == 16 else None
+    ax.scatter(n_ref, norm, color=c, marker="o", facecolor="none", label=label)
+
+for n_ref, norm in zip(n_refs + 1, dual_norms):
+    c = highlight if n_ref == 5 else "0.3"
+    label = "dual" if n_ref == 9 else None
+    ax.scatter(n_ref, norm, color=c, marker="x", label=label)
+
+for n_ref, norm in zip(n_refs + 1, left_norms):
+    c = highlight if n_ref == 5 else "0.3"
+    label = "left/right" if n_ref == 9 else None
+    ax.scatter(n_ref, norm, color=c, marker="+", label=label)
+
+for j in range(n_seed):
+    label = "random" if j == 0 else None
+    ax.plot(
+        n_refs_random,
+        random_norms[:, j],
+        color="0.8",
+        marker=".",
+        label=label,
+        zorder=0,
+    )
+
+ax.plot(
+    n_refs_optimal,
+    optimal_norms,
+    color="k",
+    linestyle="--",
+    label="optimized",
+    zorder=0,
+)
+ax.axhline(exact_norm, color="k", label="bounds")
+ax.axhline(0.0, color="k")
+
+ax.set_xscale("log")
+ax.set_yscale("linear")
+ax.set_xlabel(r"$n$")
+ax.set_ylabel(r"$\tilde{d}$")
+ax.set_xticks(major_ticks)
+ax.set_xticks(minor_ticks, minor=True)
+ax.set_xticklabels([str(m) for m in major_ticks])
+ax.set_xticklabels([], minor=True)
+# ax.legend()
+fname = os.path.join("img", "convergence-absolute.pdf")
+plt.savefig(fname=fname, bbox_inches="tight")
+plt.show()
+
+
+fig, ax = plt.subplots()
+
+for n_ref, norm in zip(n_refs_hierarchical, hierarchical_norms):
+    c = highlight if n_ref == 8 else "0.3"
+    label = "hierarchical" if n_ref == 16 else None
+    ax.scatter(
+        n_ref, exact_norm - norm, color=c, marker="o", facecolor="none", label=label
+    )
+
+for n_ref, norm in zip(n_refs + 1, dual_norms):
+    c = highlight if n_ref == 5 else "0.3"
+    label = "dual" if n_ref == 9 else None
+    ax.scatter(n_ref, exact_norm - norm, color=c, marker="x", label=label)
+
+for n_ref, norm in zip(n_refs + 1, left_norms):
+    c = highlight if n_ref == 5 else "0.3"
+    label = "left/right" if n_ref == 9 else None
+    ax.scatter(n_ref, exact_norm - norm, color=c, marker="+", label=label)
 
 for j in range(n_seed):
     label = "random" if j == 0 else None
     ax.plot(
         n_refs_random,
         exact_norm - random_norms[:, j],
-        color="0.5",
+        color="0.8",
         marker=".",
         label=label,
-        zorder=1,
+        zorder=0,
     )
 
-ax.scatter(
-    n_refs_hierarchical,
-    exact_norm - hierarchical_norms,
-    color="C0",
-    marker="o",
-    label="hierarchical",
-)
-ax.scatter(
-    n_refs + 1,
-    exact_norm - dual_norms,
-    color="C1",
-    marker="o",
-    label="dual",
-)
 ax.plot(
     n_refs_optimal,
     exact_norm - optimal_norms,
     color="k",
-    label="bounds",
+    linestyle="--",
+    label="optimized",
+    zorder=0,
 )
-ax.axhline(
-    exact_norm,
-    color="k",
-    label="",
-)
+ax.axhline(exact_norm, color="k", label="bounds")
+
+x, y = 48, 0.01
+trix, triy = [x, x * 2, x, x], [y, y / 2, y / 2, y]
+ax.plot(trix, triy, color="k")
+ax.text(x * 0.9, y / np.sqrt(2), "$1$", ha="right", va="center_baseline")
+ax.text(x * np.sqrt(2), y / 2 * 0.9, "$1$", ha="center", va="top")
 
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_xlabel(r"$n$")
 ax.set_ylabel(r"$d - \tilde{d}$")
-ax.set_xticks(n_refs)
-ax.xaxis.set_major_locator(LogLocator(base=2))
-ax.xaxis.set_major_formatter(LogFormatter(base=2))
-ax.xaxis.set_minor_locator(LogLocator(base=2, subs=(1.25, 1.5, 1.75)))
-ax.xaxis.set_minor_formatter(NullFormatter())
-ax.set_aspect("equal")
-ax.legend()
-fname = os.path.join("img", "convergence-study.pdf")
+ax.set_xticks(major_ticks)
+ax.set_xticks(minor_ticks, minor=True)
+ax.set_xticklabels([str(m) for m in major_ticks])
+ax.set_xticklabels([], minor=True)
+ax.legend(bbox_to_anchor=(1.0, 1.0), loc="upper left")
+fname = os.path.join("img", "convergence-relative.pdf")
 plt.savefig(fname=fname, bbox_inches="tight")
 plt.show()
